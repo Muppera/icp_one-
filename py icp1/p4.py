ccb6f19ac560798a769fea0921139c972e1d6189
Muppera <nmv8g@umsystem.edu>
2021-08-31 04:23:21 +0530

    Added python files

---
+++ py icp1/p4.py
@@ -0,0 +1,26 @@
+class_score = int(input("Enter the score:"))
+if class_score >=94:
+    print("A")
+elif class_score >= 90:
+        print("A-")
+elif class_score >= 87:
+        print("B+")
+elif class_score >= 84:
+        print("B")
+elif class_score >= 80:
+        print("B-")
+elif class_score >= 77:
+        print("C+")
+elif class_score >= 74:
+        print("C")
+elif class_score >= 70:
+        print("C-")
+elif class_score >= 67:
+        print("D+")
+elif class_score >= 64:
+        print("D")
+elif class_score >= 60:
+        print("D-")
+else :
+  print("F")
+
